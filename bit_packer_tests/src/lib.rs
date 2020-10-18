978ca07771515cd5dcecd07505dadb6e6d84021a
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-18 18:42:09 +1100

    test: updated test sample data

--- bit_packer_tests/src/lib.rs
+++ bit_packer_tests/src/lib.rs
@@ -1,33 +1,58 @@
 #[cfg(test)]
-mod tests {
+mod tests
+{
     #[test]
-    fn it_works() {
+    fn it_works()
+    {
         assert_eq!(2 + 2, 4);
     }
 }
 
 extern crate bit_packer;
 
-use bit_packer::BitPacker;
+use bit_packer::{BitPacker, PackedBits};
 
-struct NetPacketHeader {
+struct NetPacketHeader
+{
     protocol_id: u32,
+    // Fragment, Data, Control = 2bits
     packet_type: u8,
-    // Fragment, Data, Control
     sequence_number: u16,
+    confirmed_number: u16,
 }
 
-struct ControlPacketPayload {
+struct Packet<Coffee>
+where Coffee: BitPacker
+{
+    header: NetPacketHeader,
+    data: Coffee,
+}
+
+struct ControlPacket
+{
+    header: NetPacketHeader,
+    data: ControlPacketPayload,
+}
+
+struct ControlPacketPayload
+{
+    // Connect/Disconnect/Reject/Accept/Ack etc..
     command: u8,
-    // Connect/Disconnect/Reject/Accept etc..
-    control_data_type: u8,
     // None, something else if required, like auth token
+    control_data_type: u8,
     control_data: Box<dyn BitPacker>,
 }
 
-struct ControlPacket {
+struct Ack
+{
+    sequence_number: u16,    //500 500-1 500-2  500-8 500-32
+    previous_sequences: u32, // 0b 0000 0000 0000 0000 0000 0000 1000 0011
+}
+
+struct DataPacker
+{
     header: NetPacketHeader,
-    data: ControlPacketPayload,
+    data: DataPacketPayload,
 }
 
 struct DataPacketPayload
@@ -37,11 +62,13 @@ struct DataPacketPayload
     data: Box<dyn BitPacker>,
 }
 
-struct SnapshotHeader {
-    snapshot_list: Vec<(u8, u8)> // u8 SnapshotType, u8 Count
-}
-
-struct SnapshotPayload {
+struct SnapshotPayload
+{
     header: SnapshotHeader,
     data: Vec<Box<dyn BitPacker>>,
+}
+
+struct SnapshotHeader
+{
+    snapshot_list: Vec<(u8, u8)>, // u8 SnapshotType, u8 Count
 }
