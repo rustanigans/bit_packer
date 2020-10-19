99d87f1e038366ea3f7dc296a6c5def3a94ebf87
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-19 14:06:57 +1100

    refactor: removed comment

--- src/impls_for/packed_bits/from_bytes_and_trailing_zeros_tuple.rs
+++ src/impls_for/packed_bits/from_bytes_and_trailing_zeros_tuple.rs
@@ -1,6 +1,5 @@
 use crate::PackedBits;
 
-// TODO Move this
 impl From<(Vec<u8>, usize)> for PackedBits
 {
     fn from((bytes, tz): (Vec<u8>, usize)) -> Self { PackedBits::from_bytes_and_tz(bytes, tz) }
