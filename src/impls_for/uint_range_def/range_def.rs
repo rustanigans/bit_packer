c06d1878e0fb8c7aab4aeecca768fd462f85a037
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-19 15:10:11 +1100

    feat: add pack/restore methods to RangeDef trait

--- src/impls_for/uint_range_def/range_def.rs
+++ src/impls_for/uint_range_def/range_def.rs
@@ -6,4 +6,12 @@ impl RangeDef<u8> for UIntRangeDef<u8>
     fn new(min: u8, max: u8) -> Self{
         UIntRangeDef ::new(min, max)
     }
+
+    fn pack(val: u8) -> (Vec<u8>, usize) {
+        unimplemented!()
+    }
+
+    fn restore(val: Vec<u8>) -> u8 {
+        unimplemented!()
+    }
 }
--- src/traits/range_def.rs
+++ src/traits/range_def.rs
@@ -1,3 +1,6 @@
-pub trait RangeDef<T>{
-    fn new(min:T, max:T) -> Self;
+pub trait RangeDef<T>
+{
+    fn new(min: T, max: T) -> Self;
+    fn pack(val: T) -> (Vec<u8>, usize);
+    fn restore(val: Vec<u8>) -> T;
 }
