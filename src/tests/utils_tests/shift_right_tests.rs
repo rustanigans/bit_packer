84c9aa9d26b13b85494de7359a0a181dfcfa32bf
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-19 12:00:19 +1100

    fix: incorrect trailing zeros in shift right
    also added test

--- src/tests/utils_tests/shift_right_tests.rs
+++ src/tests/utils_tests/shift_right_tests.rs
@@ -2,18 +2,19 @@ use crate::utils::shift_right::shift_right;
 
 macro_rules! test_shift_right {
 ($($name:ident, $shift: expr, $tz: expr, $value: expr, $expected: expr, $expected_tz:expr)*) =>
-{
-    $(
-        #[test]
-        fn $name() {
-            let mut bytes  = $value;
-            let result = shift_right(&mut bytes, $shift, $tz);
-            assert_eq!($expected, &bytes[..]);
-            assert_eq!($expected_tz, result);
-        }
-    )*
+    {
+        $(
+            #[test]
+            fn $name() {
+                let mut bytes  = $value;
+                let result = shift_right(&mut bytes, $shift, $tz);
+                assert_eq!($expected, &bytes[..]);
+                assert_eq!($expected_tz, result);
+            }
+        )*
+    }
 }
-}
+
 test_shift_right!(
     shift_8bits_by_8bits, 8, 0, vec![255], &[0,255], 0
     shift_4bits_by_8bits, 8, 4, vec![0b11110000], &[0,0b11110000], 4
@@ -25,4 +26,5 @@ test_shift_right!(
     shift_12bits_by_4bits, 4, 4, vec![255,0b11110000], &[0b00001111,255], 0
     shift_8bits_by_1bits, 1, 0, vec![255], &[255>>1,128], 7
     shift_2bits_by_3bits, 3, 6, vec![0b11000000], &[0b00011000], 3
+    shift_15bits_by_15bits, 15, 1, vec![255,254], &[0,1,255,252], 2
 );
--- src/utils/shift_right.rs
+++ src/utils/shift_right.rs
@@ -1,4 +1,4 @@
-pub(crate) fn shift_right(bytes: &mut Vec<u8>, count: usize, mut trailing_zeros: usize) -> usize
+pub(crate) fn shift_right(bytes: &mut Vec<u8>, mut count: usize, mut trailing_zeros: usize) -> usize
 {
     if count == 0
     {
@@ -6,7 +6,7 @@ pub(crate) fn shift_right(bytes: &mut Vec<u8>, count: usize, mut trailing_zeros:
     }
 
     let pad_whole_bytes = count / 8;
-    let count = count - pad_whole_bytes * 8;
+    count -= pad_whole_bytes * 8;
     {
         let mut new_bytes = vec![0u8; pad_whole_bytes];
         new_bytes.append(bytes);
@@ -18,16 +18,12 @@ pub(crate) fn shift_right(bytes: &mut Vec<u8>, count: usize, mut trailing_zeros:
         return trailing_zeros;
     }
 
-    // 5 > 3 = 2 tz  7 > 3 = 4 tz  1 > 0 =7tz  2> 0 6
     if count > trailing_zeros
     {
         bytes.push(0);
     }
-    if trailing_zeros == 0 {
-        trailing_zeros = 8;
-    }
-    // 3 < 5 = 2tz  3 < 7 = 4tx   1 < 7 =
-    trailing_zeros = (count as isize - trailing_zeros as isize).abs() as usize;
+
+    trailing_zeros = 8 - count + trailing_zeros;   //  (count as isize - trailing_zeros as isize).abs() as usize;
 
     let mut trimmed = 0;
 
@@ -42,5 +38,5 @@ pub(crate) fn shift_right(bytes: &mut Vec<u8>, count: usize, mut trailing_zeros:
 
         *item = new_item;
     }
-    trailing_zeros
+    trailing_zeros % 8
 }
