dd98af2aa7bec1930e99c5513d5ebb19fa7a800c
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-19 17:14:58 +1100

    removed placeholder code

--- src/traits/bit_packer.rs
+++ src/traits/bit_packer.rs
@@ -3,15 +3,3 @@ use crate::{PackedBits};
 pub trait BitPacker {
     fn add_to_packed_bits(self, bits: &mut PackedBits);
 }
-
-pub trait PPush<T> {
-    fn push(&mut self, val: T) ;
-}
-
-impl<T> PPush<T> for PackedBits
-    where T: BitPacker
-{
-    fn push(&mut self, val: T) {
-        val.add_to_packed_bits(self)
-    }
-}
