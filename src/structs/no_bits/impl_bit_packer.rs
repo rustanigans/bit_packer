57b3140dc79640f8a64c2c4155bde81a32bfbd22
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-20 11:03:45 +1100

    updated BitPacker for NoBits impl

--- src/structs/no_bits/impl_bit_packer.rs
+++ src/structs/no_bits/impl_bit_packer.rs
@@ -3,4 +3,6 @@ use crate::{BitPacker, NoBits, PackedBits};
 impl BitPacker for NoBits
 {
     fn add_to_packed_bits(&self, _bits: &mut PackedBits) {}
+
+    fn extract_from_packed_bits(&mut self, _bits: &mut PackedBits) {}
 }
