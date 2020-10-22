f5415791516f678888beae52253c88ae6712215d
Sibz <12050358+Sibz@users.noreply.github.com>
2020-10-22 12:33:24 +1100

    added BitPacker impl for Vec<Box<dyn BitPacker>>

---
+++ src/impls_for/bit_packer_for_vec_box_dyn_bit_packer.rs
@@ -0,0 +1,15 @@
+use crate::{BitPacker, PackedBits, Push};
+
+impl BitPacker for Vec<Box<dyn BitPacker>> {
+    fn add_to_packed_bits(&self, bits: &mut PackedBits)
+    {
+        for item in self.iter() {
+            bits.push(item);
+        }
+    }
+
+    fn extract_from_packed_bits(&mut self, bits: &mut PackedBits)
+    {
+        panic!("Cannot extract Vec<Box<dyn BitPacker>> automatically");
+    }
+}
