544c82e7fcfdde0ef4c84652b2c1a928f4f941b4
Sibz <12050358+Sibz@users.noreply.github.com>
2020-11-02 20:52:31 +1100

    refactor(PackedBits): deprecated shift trait fn

--- src/traits/shift.rs
+++ src/traits/shift.rs
@@ -2,5 +2,7 @@ use crate::BitPacker;
 
 pub trait Shift: Send
 {
+    #[deprecated(since = "0.1.1",
+                 note = "Use PackedBits.take_front instead. If you must use this (i.e. you need to use a 'dyn Shift') let @Sibz know of the use case")]
     fn shift(&mut self, val: &mut dyn BitPacker);
 }
